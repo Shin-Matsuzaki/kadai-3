84a861e97385a822a5c7f5f5f083f5d4127038b4
matsuzaki <shin.matsu.p@gmail.com>
2019-09-25 16:16:13 +0900

    C-5: NGワードとCensorワードを，入力を受け付けて変更できる仕様に変更
    - detectの使い方があまり有効的ではないため，detectとcensorの機能をまとめてメソッド化した方が，forループが1回で済みキレイかもしれない

--- wordfilter.py
+++ wordfilter.py
@@ -3,30 +3,37 @@ class WordFilter(object):
         self.ng_list = word_list
 
     def detect(self, text):
+        # NGワードを1つずつ確認
         for ng in self.ng_list:
+            # 1つでも合致したらTrueを返す
             if ng in text:
                 return True
         return False
 
-    def censor(self, text, censor_word):
+    def censor(self, text, censor):
         text_censored = text
+        # 1つでもNGワードが見つかれば，replaceを実行
         if self.detect(text):
             for ng in self.ng_list:
-                text_censored = text_censored.replace(ng, censor_word)
+                text_censored = text_censored.replace(ng, censor)
         return text_censored
 
 
 def main():
-    my_filter = WordFilter(["アーセナル", "リバプール"])
-    flag = my_filter.censor("昨日のアーセナルの試合アツかった", "<censored>")
+    ng_word = input('NGワードを入力してください(スペース区切り):')
+    ng_list = list(map(str, ng_word.split(' ')))
+    censor_word = input('Censorワードを入力してください:')
+
+    my_filter = WordFilter(ng_list)
+    flag = my_filter.censor("昨日のアーセナルの試合アツかった", censor_word)
     print(flag)
 
-    flag2 = my_filter.censor("昨日のリバプールの試合アツかった", "<censored>")
+    flag2 = my_filter.censor("昨日のリバプールの試合アツかった", censor_word)
     print(flag2)
 
-    flag3 = my_filter.censor("昨日のACミランの試合アツかった", "<censored>")
+    flag3 = my_filter.censor("昨日のACミランの試合アツかった", censor_word)
     print(flag3)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
