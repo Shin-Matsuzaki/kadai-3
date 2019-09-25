bb2ddd6bfa166f413d7a0a26a92dacedfded9a36
matsuzaki <shin.matsu.p@gmail.com>
2019-09-25 15:16:51 +0900

    C-3: NGワードを置換する文字列を任意に変更できるよう拡張

--- wordfilter.py
+++ wordfilter.py
@@ -5,18 +5,18 @@ class WordFilter(object):
     def detect(self, text):
         return self.ng in text
 
-    def censor(self, text):
+    def censor(self, text, censor_word):
         if self.detect(text):
-            return text.replace(self.ng, '<censored>')
+            return text.replace(self.ng, censor_word)
         return text
 
 
 def main():
     my_filter = WordFilter('アーセナル')
-    flag = my_filter.censor("昨日のアーセナルの試合アツかった")
+    flag = my_filter.censor("昨日のアーセナルの試合アツかった", "ミラン")
     print(flag)
 
-    flag2 = my_filter.censor("昨日のリバプールの試合アツかった")
+    flag2 = my_filter.censor("昨日のリバプールの試合アツかった", "ミラン")
     print(flag2)
 
 
