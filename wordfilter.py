7b8fe57882c293f4838dd5f95692e8a13df03898
matsuzaki <shin.matsu.p@gmail.com>
2019-09-25 15:51:20 +0900

    C-4: 複数のNGワードを登録できるように拡張
    - NGワードをリストで与える仕様にした

--- wordfilter.py
+++ wordfilter.py
@@ -1,23 +1,31 @@
 class WordFilter(object):
-    def __init__(self, word):
-        self.ng = word
+    def __init__(self, word_list):
+        self.ng_list = word_list
 
     def detect(self, text):
-        return self.ng in text
+        for ng in self.ng_list:
+            if ng in text:
+                return True
+        return False
 
     def censor(self, text, censor_word):
+        text_censored = text
         if self.detect(text):
-            return text.replace(self.ng, censor_word)
-        return text
+            for ng in self.ng_list:
+                text_censored = text_censored.replace(ng, censor_word)
+        return text_censored
 
 
 def main():
-    my_filter = WordFilter('アーセナル')
-    flag = my_filter.censor("昨日のアーセナルの試合アツかった", "ミラン")
+    my_filter = WordFilter(["アーセナル", "リバプール"])
+    flag = my_filter.censor("昨日のアーセナルの試合アツかった", "<censored>")
     print(flag)
 
-    flag2 = my_filter.censor("昨日のリバプールの試合アツかった", "ミラン")
+    flag2 = my_filter.censor("昨日のリバプールの試合アツかった", "<censored>")
     print(flag2)
+
+    flag3 = my_filter.censor("昨日のACミランの試合アツかった", "<censored>")
+    print(flag3)
 
 
 if __name__ == "__main__":
